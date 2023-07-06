e890446938b0fa6727093bfa87af8ba7eefa1f29
5HT <maxim@synrc.com>
2023-07-06 04:19:51 +0300

    cofactor is irrelevant

--- priv/certs/cms-encrypt.sh
+++ priv/certs/cms-encrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-openssl cms -encrypt -aes256 -in message.txt -out encrypted.txt -recip client.pem -keyopt ecdh_kdf_md:sha256 -keyopt ecdh_cofactor_mode:0
+openssl cms -encrypt -aes256 -in message.txt -out encrypted.txt -recip client.pem -keyopt ecdh_kdf_md:sha256
