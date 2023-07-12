2b593f9e413d6638e7efc60d33a9d49190e6b14b
5HT <maxim@synrc.com>
2023-07-12 17:47:10 +0300

    cache binary

--- lib/CHAT.ex
+++ lib/CHAT.ex
@@ -5,16 +5,21 @@ defmodule CHAT.CRYPTO do
     def public(name), do: e(3,e(8, e(2, :public_key.pem_entry_decode(readPEM("priv/certs/",name)))))
     def readPEM(folder, name),     do: hd(:public_key.pem_decode(e(2, :file.read_file(folder <> name))))
     def shared(pub, key, scheme),  do: :crypto.compute_key(:ecdh, pub, key, scheme)
-    def eccCMS(ukm, len), do: {:'ECC-CMS-SharedInfo', {:'KeyWrapAlgorithm',{2,16,840,1,101,3,4,1,45},:asn1_NOVALUE}, ukm, <<len::32>>}
+    def eccCMS(ukm, len), do: {:'ECC-CMS-SharedInfo',
+        {:'KeyWrapAlgorithm',{2,16,840,1,101,3,4,1,45},:asn1_NOVALUE}, ukm, <<len::32>>}
+
+    def selector({1,3,132,0,34}), do: :secp384r1
 
     def testSMIME() do
         {:ok,base} = :file.read_file "priv/certs/encrypted.txt" ; [_,s] = :string.split base, "\n\n"
-        :file.write_file "priv/certs/encrypted.bin", [:base64.decode(s)]
-        :'CryptographicMessageSyntax-2010'.decode(:ContentInfo, :base64.decode(s))
+        x = :base64.decode(s)
+        :file.write_file "priv/certs/encrypted.bin", [x]
+        :'CryptographicMessageSyntax-2010'.decode(:ContentInfo, x)
     end
 
     def testCMS() do
         privateKey = privat "client.key"
+        :io.format '~p~n', [:public_key.pem_entry_decode(readPEM("priv/certs/","client.key"))]
         scheme = :secp384r1
         {_,{:ContentInfo,_,{:EnvelopedData,_,_,x,{:EncryptedContentInfo,_,{_,_,{_,iv}},data},_}}} = testSMIME()
         [{:kari,{_,:v3,{_,{_,_,publicKey}},ukm,_,[{_,_,encryptedKey}]}}|_] = x
