6104471f660471e51724ea2808a3257715dec153
5HT <maxim@synrc.com>
2023-08-12 17:14:42 +0300

    Swift ASN.1 emiters

--- lib/chat_asn1.ex
+++ lib/chat_asn1.ex
@@ -2,50 +2,79 @@ defmodule CHAT.ASN1 do
 
   def dir(), do: :application.get_env(:ca, :bundle, "priv/apple/")
 
+  def emitDecoder(body), do:
+    """
+    @inlinable
+    init(derEncoded root: ASN1Node, withIdentifier identifier: ASN1Identifier) throws {
+        self = try DER.sequence(root, identifier: identifier) { nodes in
+            #{body}
+        }
+    }
+    """
+
+  def emitEncoder(body), do:
+    """
+    @inlinable
+    func serialize(into coder: inout DER.Serializer, withIdentifier identifier: ASN1Identifier) throws {
+        try coder.appendConstructedNode(identifier: identifier) { coder in
+            #{body}
+        }
+    }
+    """
+
+
+  def emitCtor(params, body), do:
+    """
+    @inlinable
+    init(#{params}) {
+         #{body}
+    }
+    """
+
   def compile_all() do
       {:ok, files} = :file.list_dir dir()
       :lists.map(fn file ->
-         :io.format 'file: ~p~n', [file]
+         :logger.info 'file: ~p~n', [file]
          parse(dir() <> :erlang.list_to_binary(file))
       end, files)
       :ok
   end
 
   def dumpType(pos, name, type) do
-      :io.format '~p~n', [type]
-      :io.format 'name: ~p~n', [name]
-      :io.format 'pos: ~p~n', [pos]
+      :logger.info '~p~n', [type]
+      :logger.info 'name: ~p~n', [name]
+      :logger.info 'pos: ~p~n', [pos]
   end
 
   def dumpValue(pos, name, type, value, mod) do
-      :io.format '~p~n', [type]
-      :io.format 'name: ~p~n', [name]
-      :io.format 'value: ~p~n', [value]
-      :io.format 'mod: ~p~n', [mod]
-      :io.format 'pos: ~p~n', [pos]
+      :logger.info '~p~n', [type]
+      :logger.info 'name: ~p~n', [name]
+      :logger.info 'value: ~p~n', [value]
+      :logger.info 'mod: ~p~n', [mod]
+      :logger.info 'pos: ~p~n', [pos]
   end
 
   def dumpClass(pos, name, mod, type) do
-      :io.format '~p~n', [type]
-      :io.format 'name: ~p~n', [name]
-      :io.format 'mod: ~p~n', [mod]
-      :io.format 'pos: ~p~n', [pos]
+      :logger.info '~p~n', [type]
+      :logger.info 'name: ~p~n', [name]
+      :logger.info 'mod: ~p~n', [mod]
+      :logger.info 'pos: ~p~n', [pos]
   end
 
   def dumpPType(pos, name, args, type) do
-      :io.format '~p~n', [type]
-      :io.format 'name: ~p~n', [name]
-      :io.format 'args: ~p~n', [args]
-      :io.format 'pos: ~p~n', [pos]
+      :logger.info '~p~n', [type]
+      :logger.info 'name: ~p~n', [name]
+      :logger.info 'args: ~p~n', [args]
+      :logger.info 'pos: ~p~n', [pos]
   end
 
   def dumpModule(pos, name, defid, tagdefault, exports, imports) do
-      :io.format 'pos: ~p~n', [pos]
-      :io.format 'name: ~p~n', [name]
-      :io.format 'defid: ~p~n', [defid]
-      :io.format 'tagdefault: ~p~n', [tagdefault]
-      :io.format 'exports: ~p~n', [exports]
-      :io.format 'imports: ~p~n', [imports]
+      :logger.info 'pos: ~p~n', [pos]
+      :logger.info 'name: ~p~n', [name]
+      :logger.info 'defid: ~p~n', [defid]
+      :logger.info 'tagdefault: ~p~n', [tagdefault]
+      :logger.info 'exports: ~p~n', [exports]
+      :logger.info 'imports: ~p~n', [imports]
   end
 
   def parse(file \\ "priv/proto/CHAT.asn1") do
