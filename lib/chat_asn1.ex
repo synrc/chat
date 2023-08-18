cea67bfea0f6d3a05996cf6e451ed2bd1724f9d2
5HT <maxim@synrc.com>
2023-08-18 03:26:52 +0300

    RDN Attr

--- lib/chat_asn1.ex
+++ lib/chat_asn1.ex
@@ -20,6 +20,8 @@ import Crypto
 """
   end
 
+  def parseFieldName({:contentType, {:Externaltypereference,_,moduleFile, fieldName}}), do: "#{fieldName}"
+  def parseFieldName(fieldName),                                                        do: "#{fieldName}"
   def parseFieldType(fieldType) when is_atom(fieldType),                                do: "#{fieldType}"
   def parseFieldType({:ANY_DEFINED_BY, fieldType}) when is_atom(fieldType),             do: "#{fieldType}"
   def parseFieldType({:contentType, {:Externaltypereference,_,moduleFile, fieldType}}), do: "#{fieldType}"
@@ -29,9 +31,6 @@ import Crypto
   def parseFieldType({:"SET OF",{:type,_,{:"SEQUENCE", _, _, _,fieldTypes},_,_,_}}),    do: 
       Enum.join(:lists.map(fn x -> parseFieldType(x) end, fieldTypes), "->")
   def parseFieldType({:"SET OF",{:type,_,external,_,_,_}}), do: parseFieldType(external)
-
-  def parseFieldName({:contentType, {:Externaltypereference,_,moduleFile, fieldName}}), do: "#{fieldName}"
-  def parseFieldName(fieldName),                                                        do: "#{fieldName}"
 
   def emitFields(pad, fields) when is_list(fields) do
       Enum.join(:lists.map(fn 
@@ -132,7 +131,6 @@ import Crypto
            {:type, _, :"OBJECT IDENTIFIER", _, _, :no} -> 
                :skip
            {:type, _, {typeASN1, _, _, _, fields}, _, _, :no} -> 
-               :io.format 'args: ~p', [fields]
                res = case typeASN1 do
                  :SEQUENCE ->
                     emitSequenceDefinition(
@@ -143,48 +141,22 @@ import Crypto
                        emitSequenceEncoder(emitEncoderBody(fields)))
                  _ -> :logger.info('ASN.1 type ~p is not supported.', [typeASN1])
                end
-               file = normalizeName(name)
-               :file.write_file file <> ".swift", res
+               file = normalizeName(name) <> ".swift"
+               :logger.info 'write: ~p', [file]
+               :file.write_file file, res
       end 
   end
 
   def normalizeName(name), do: Enum.join(String.split("#{name}", "-"), "")
 
-  def dumpValue(pos, name, type, value, mod) do
-      :logger.info 'value ~p', [type]
-      :logger.info 'value name: ~p', [name]
-      :logger.info 'value value: ~p', [value]
-      :logger.info 'value mod: ~p', [mod]
-      :logger.info 'value pos: ~p', [pos]
-  end
-
-  def dumpClass(pos, name, mod, type) do
-      :logger.info 'class ~p', [type]
-      :logger.info 'class name: ~p', [name]
-      :logger.info 'class mod: ~p', [mod]
-      :logger.info 'class pos: ~p', [pos]
-  end
-
-  def dumpPType(pos, name, args, type) do
-      :logger.info 'ptype ~p', [type]
-      :logger.info 'ptype name: ~p', [name]
-      :logger.info 'ptype args: ~p', [args]
-      :logger.info 'ptype pos: ~p', [pos]
-  end
-
-  def dumpModule(pos, name, defid, tagdefault, exports, imports) do
-      :logger.info 'module pos: ~p', [pos]
-      :logger.info 'module name: ~p', [name]
-      :logger.info 'module defid: ~p', [defid]
-      :logger.info 'module tagdefault: ~p', [tagdefault]
-      :logger.info 'module exports: ~p', [exports]
-      :logger.info 'module imports: ~p', [imports]
-  end
+  def dumpValue(pos, name, type, value, mod), do: []
+  def dumpClass(pos, name, mod, type), do: []
+  def dumpPType(pos, name, args, type), do: []
+  def dumpModule(pos, name, defid, tagdefault, exports, imports), do: []
 
   def compile(file \\ "priv/proto/CHAT.asn1") do
       tokens = :asn1ct_tok.file file
       {:ok, mod} = :asn1ct_parser2.parse file, tokens
-      :io.format '~p~n', [mod]
       {:module, pos, name, defid, tagdefault, exports, imports, _, typeorval} = mod
       :lists.map(fn
          {:typedef,  _, pos, name, type} -> compileType(pos, name, type)
