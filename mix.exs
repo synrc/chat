ccb462cc451379b12c537c4cc3f71b7772927290
5HT <maxim@synrc.com>
2026-01-05 17:00:53 +0200

    wip

--- mix.exs
+++ mix.exs
@@ -1,33 +1,38 @@
 defmodule CHAT.Mixfile do
   use Mix.Project
 
-  def application(), do:
-   [  mod: {CHAT, []},
-      applications: [ :ca ],
-   ]
+  def application() do
+      [
+        mod: {CHAT, []},
+        extra_applications: [:x509, :bandit, :plug, :logger, :ca ]
+      ]
+  end
 
   def project do
-    [app: :chat,
-     version: "9.1.1",
-     description: "CHAT  CXC 138 25 X.509 CMS Instant Messenger",
-     package: package(),
-     deps: deps()]
+      [
+        app: :chat,
+        version: "9.1.1",
+        description: "CHAT  CXC 138 25 X.509 CMS Instant Messenger",
+        package: package(),
+        deps: deps()
+      ]
   end
 
   def package() do
-    [
-      files: ["src", "include", "LICENSE", "README.md" ],
-      licenses: ["DHARMA"],
-      maintainers: ["Namdak Tonpa"],
-      name: :chat,
-      links: %{"GitHub" => "https://github.com/synrc/chat"}
-    ]
+      [
+        files: ["src", "include", "LICENSE", "README.md" ],
+        licenses: ["DHARMA"],
+        maintainers: ["Namdak Tonpa"],
+        name: :chat,
+        links: %{"GitHub" => "https://github.com/synrc/chat"}
+      ]
   end
 
   def deps() do
-    [ {:ex_doc, ">= 0.0.0", only: :dev},
-      {:ca, "~> 7.1.2"},
-      {:ssl_verify_fun, "~> 1.1.7"}
-    ]
+      [
+        {:ex_doc, ">= 0.0.0", only: :dev},
+        {:ca, "~> 7.1.1"},
+        {:ssl_verify_fun, "~> 1.1.7"}
+      ]
   end
 end
