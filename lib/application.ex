19315e7b20116af774a6c65e781eb4fbf2e8810c
5HT <maxim@synrc.com>
2026-02-28 23:51:11 +0200

    wip

--- lib/application.ex
+++ lib/application.ex
@@ -11,25 +11,27 @@ defmodule CHAT do
 
   def metainfo() do
       KVS.schema(name: :"CHAT-v2", tables: [
-       KVS.table(name: :Authority, fields: [:id,:vsn,:session,:type,:cert,:settings], instance: CHAT.X509."Authority"()),
-       KVS.table(name: :Message,   fields: [:id,:vsn,:session,:inbox,:from,:to,:files,:type,:link,
-                                            :seenby,:repliedby,:mentioned,:status], instance: CHAT.X509."Message"()),
+       KVS.table(name: :Authority, fields: [
+         :id,:vsn,:session,:type,:cert,:settings], instance: CHAT.X509."Authority"()),
+       KVS.table(name: :Message,   fields: [
+         :id,:vsn,:session,:inbox,:from,:to,:files,:type,:link,
+         :seenby,:repliedby,:mentioned,:status], instance: CHAT.X509."Message"()),
       ])
   end
 
+  @c1_port 17000
   @p1_port 17001
   @p3_port 17002
   @p7_port 17003
-  @c1_port 17004
 
   def start(_type, _args) do
       :logger.add_handlers(:chat)
       Supervisor.start_link([
          { Task.Supervisor, name: CHAT.TaskSupervisor},
+         { CHAT.X509,    port: @c1_port}  # CHAT C1 SPEC: Instant Messaging and Command and Control
          { MAIL.X420.P1, port: @p1_port}, # MMHS P1 SPEC: MTA-to-MTA transfer (relay).
          { MAIL.X420.P3, port: @p3_port}, # MMHS P3 SPEC: UA-to-MTA submission and MTA-to-UA delivery.
          { MAIL.X420.P7, port: @p7_port}, # MMHS P7 SPEC: UA-to-MS retrieval (with MTA delivering to MS via P3-like internal mechanism).
-         { CHAT.X509,    port: @c1_port}  # CHAT C1 SPEC: Instant Messaging and Command and Control
       ], strategy: :one_for_one, name: CHAT.Supervisor)
   end
 
